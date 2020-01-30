0230925f71ee1f8e68c4feb8191c9def978b8dc9
Alikhan Batchaev <loko-han@mail.ru>
2020-01-30 23:56:29 +0300

    распечатываю сумму чаевых выбранную пользователем при нажатии кнопки

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -16,13 +16,31 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
+    var tip = 0.10
+    
     
     @IBAction func tipChanged(_ sender: UIButton) {
-        zeroPctButton.isSelected = true
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        sender.isSelected = true
+        
+        let buttonTitle = sender.currentTitle!
+        
+        // Удаляем последний символ (%) из заголовка, затем превращаем его обратно в строку.
+        let buttonTitleMinusPercentSign = String(buttonTitle.dropLast())
+        let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
+        
+        // Разделите процент, выраженный из 100, в десятичную, например, 10 становится 0,1
+        tip = buttonTitleAsANumber / 100
     }
+    
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
     }
+    
     @IBAction func calculatePressed(_ sender: UIButton) {
+        print(tip)
     }
     
 }
