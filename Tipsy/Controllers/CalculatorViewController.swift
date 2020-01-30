1a54dc4aeba6ff827e968a297eba669e0e1cba42
Alikhan Batchaev <loko-han@mail.ru>
2020-01-31 01:32:43 +0300

    Умножаем счет на процент чаевых и делим на количество людей, чтобы разделить счет.

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -18,9 +18,13 @@ class CalculatorViewController: UIViewController {
     
     var tip = 0.10
     var numberOfPeople = 2
+    var billTotal = 0.0
     
     
     @IBAction func tipChanged(_ sender: UIButton) {
+        
+        billTextField.endEditing(true)
+        
         zeroPctButton.isSelected = false
         tenPctButton.isSelected = false
         twentyPctButton.isSelected = false
@@ -34,6 +38,8 @@ class CalculatorViewController: UIViewController {
         
         // Divide the percentage, expressed from 100, in decimal
         tip = buttonTitleAsANumber / 100
+        
+        
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
@@ -41,10 +47,21 @@ class CalculatorViewController: UIViewController {
         splitNumberLabel.text = String(format: "%.0f", sender.value)
         // Set the stepper value as an  whole number
         numberOfPeople = Int(sender.value)
+        
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
-        print(numberOfPeople)
+        let bill = billTextField.text!
+        
+        if bill != "" {
+            billTotal = Double(bill)!
+            //Multiply the bill by the tip percentage and divide by the number of people to split the bill.
+            let result = billTotal * (1 + tip) / Double(numberOfPeople)
+            //Round the result to 2 decimal places and turn it into a String.
+            let resultTo2DecimalPlaces = String(format: "%.2f", result)
+            print(resultTo2DecimalPlaces)
+        }
+        
     }
     
 }
