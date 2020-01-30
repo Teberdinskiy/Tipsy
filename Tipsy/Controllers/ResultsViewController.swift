48dea8357cd24b2fd8acbd1c0d39aaaf4e287e79
Alikhan Batchaev <loko-han@mail.ru>
2020-01-30 22:53:51 +0300

    добавил аутлеты и айбиэкшины

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by 🧔🏻Alikhan Batchaev on 30.01.2020.
+//  Copyright © 2020 Alikhan Batchaev. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+    
+    
+}
